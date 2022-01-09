c271c69f41c4d44ef32221fe8b1d911d868460fb
yuyu <hcsd7b34@gmail.com>
2022-01-10 04:12:20 +0900

    基本ファイルを作成

---
+++ window.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1500,
+        height: 900,
+    });
+
+    mainWindow.loadFile('index.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
