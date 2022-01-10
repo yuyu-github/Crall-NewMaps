4c14589f09de9a0b06a7adafdd560c56a82ec62f
yuyu <hcsd7b34@gmail.com>
2022-01-10 21:33:32 +0900

    背景に地理院地図を表示できる機能を追加

--- js/background.js
+++ js/background.js
@@ -3,47 +3,72 @@ export let backgroundObj = null;
 
 export function setEvents() {
     document.querySelector('#background-setting > select').addEventListener('change', e => {
+        function loadLeaflet() {
+            if (document.getElementById('leaflet-style') == null) {
+                let el = document.createElement('link');
+                el.id = 'leaflet-style';
+                el.rel = 'stylesheet';
+                el.href = 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css';
+                document.head.appendChild(el);
+            }
+            if (document.getElementById('leaflet-script') == null) {
+                let el = document.createElement('script');
+                el.id = 'leaflet-script';
+                el.src = 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js';
+                document.head.appendChild(el);
+            }
+        }
+
+        backgroundObj?.remove();
+        document.getElementById('background').innerHTML = '';
+        document.getElementById('background').getAttributeNames().forEach(item => {
+            if (item != 'id') document.getElementById('background').removeAttribute(item);});
         background = e.currentTarget.value;
-        switch (e.currentTarget.value) {
+        backgroundObj = null;
+
+        let id;
+        switch (background) {
             case '':
-                document.getElementById('background').innerHTML = '';
-                backgroundEl = null;
                 break;
             case 'openstreetmap':
-                //スタイルとスクリプトの読み込み
-                if (document.getElementById('leaflet-style') == null) {
-                    let el = document.createElement('link');
-                    el.id = 'leaflet-style';
-                    el.rel = 'stylesheet';
-                    el.href = 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css';
-                    document.head.appendChild(el);
-                }
-                if (document.getElementById('leaflet-script') == null) {
-                    let el = document.createElement('script');
-                    el.id = 'leaflet-script';
-                    el.src = 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js';
-                    document.head.appendChild(el);
-                }
-                //地図読み込み
-                function tryInitMap() {
+                loadLeaflet();
+                //スクリプトが読み込めるまで繰り返す
+                id = setInterval(() => {
                     try {
                         //Lがあるか確認
                         L;
                         //インターバルを削除
                         clearInterval(id);
 
+                        //地図読み込み
                         backgroundObj = L.map('background', {
                             center: [35.6809591, 139.7673068],
                             zoom: 17,
-                        })
+                        });
                         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                             attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
                         }).addTo(backgroundObj);
                     }
                     catch {}
-                }
-                //スクリプトが読み込めるまで繰り返す
-                let id = setInterval(tryInitMap, 5);
+                }, 5);
+                break;
+            case 'gsi-map':
+                loadLeaflet();
+                id = setInterval(() => {
+                    try {
+                        L;
+                        clearInterval(id);
+
+                        backgroundObj = L.map('background', {
+                            center: [35.6809591, 139.7673068],
+                            zoom: 17,
+                        });
+                        L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+                            attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+                        }).addTo(backgroundObj);
+                    }
+                    catch{}
+                }, 5);
                 break;
         }
     })
