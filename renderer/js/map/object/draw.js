ca6a2bf4e86616ba57ac7af19292e96a1f302b86
yuyu <hcsd7b34@gmail.com>
2022-02-21 22:16:42 +0900

    Change: ラインを描画する要素を変更
    複数のlineからpolylineに変更

--- renderer/js/map/object/draw.js
+++ renderer/js/map/object/draw.js
@@ -26,78 +26,117 @@ export function draw(object) {
     }
     case 'line': {
       let returnValue = [[], []];
-      let previousPoint = null;
-      [...object.linkedPoints, ...(object.isPreview ? [null] : []) /*プレビューなら一要素追加*/].forEach((hash, i) => {
-        let point = points[hash];
-        if (!(object.isPreview && i == object.linkedPoints.length)) returnValue[1].push(addSVGElement('circle', {
-          'class': className,
-          'cx': elCenterX + (point.x - centerX) * 2 ** zoomLevel + 'px',
-          'cy': elCenterY + (point.y - centerY) * 2 ** zoomLevel + 'px',
-          'r': '4px',
-          'stroke': 'black',
-          'stroke-width': '0.5px',
-          'fill': 'lightgray',
-        }, 20003));
-        if (previousPoint != null) {
-          returnValue[0].push(addSVGElement('line', {
+      
+      if (object.linkedPoints.length != 0) {
+        let SVGPoints = '';
+        let lastPoint = [];
+
+        [...object.linkedPoints, ...(object.isPreview ? [null] : []) /*プレビューなら一要素追加*/].forEach((hash, i) => {
+          let point = points[hash];
+
+          if (!(object.isPreview && i == object.linkedPoints.length)) returnValue[1].push(addSVGElement('circle', {
             'class': className,
-            'x1': elCenterX + (previousPoint.x - centerX) * 2 ** zoomLevel + 'px',
-            'y1': elCenterY + (previousPoint.y - centerY) * 2 ** zoomLevel + 'px',
-            'x2': elCenterX + ((object.isPreview && i == object.linkedPoints.length ? object.previewX : point.x) - centerX) * 2 ** zoomLevel + 'px',
-            'y2': elCenterY + ((object.isPreview && i == object.linkedPoints.length ? object.previewY : point.y) - centerY) * 2 ** zoomLevel + 'px',
+            'cx': elCenterX + (point.x - centerX) * 2 ** zoomLevel + 'px',
+            'cy': elCenterY + (point.y - centerY) * 2 ** zoomLevel + 'px',
+            'r': '4px',
+            'stroke': 'black',
+            'stroke-width': '0.5px',
+            'fill': 'lightgray',
+          }, 20003));
+
+          //プレビューしている線の場合、色を薄くする
+          if (object.isPreview && i == object.linkedPoints.length) {
+            //最後の線のみ別で描画
+            returnValue[0][1] = addSVGElement('line', {
+              'class': className,
+              'x1': elCenterX + (lastPoint.x - centerX) * 2 ** zoomLevel + 'px',
+              'y1': elCenterY + (lastPoint.y - centerY) * 2 ** zoomLevel + 'px',
+              'x2': elCenterX + (object.previewX - centerX) * 2 ** zoomLevel + 'px',
+              'y2': elCenterY + (object.previewY - centerY) * 2 ** zoomLevel + 'px',
+              'stroke': 'blue',
+              'stroke-width': '4px',
+              'stroke-opacity': '0.5',
+              'style': 'pointer-events: none;',
+            }, 20000);
+          } else {
+            SVGPoints += `${elCenterX + (point.x - centerX) * 2 ** zoomLevel},${elCenterY + (point.y - centerY) * 2 ** zoomLevel} `;
+            lastPoint = { x: point.x, y: point.y };
+          }
+        });
+
+        if (object.linkedPoints.length > 1) {
+          returnValue[0][0] = addSVGElement('polyline', {
+            'class': className,
+            'points': SVGPoints,
             'stroke': 'blue',
             'stroke-width': '4px',
-            'stroke-opacity': object.isPreview && i == object.linkedPoints.length ? '0.5' : '1',
-            'style': object.isPreview && i == object.linkedPoints.length ? 'pointer-events: none;' : '',
-          }, 20001));
+            'fill': 'none',
+          }, 20000);
         }
-        previousPoint = point;
-      });
+      }
+
       return returnValue;
     }
     case 'area': {
       let returnValue = [[], []];
-      let SVGPoints = '';
-      let previousPoint = null;
-      [...object.linkedPoints, ...(object.isPreview ? [null] : [])].forEach((hash, i) => {
-        let point = points[hash];
-        if (!(object.isPreview && i == object.linkedPoints.length)) returnValue[1].push(addSVGElement('circle', {
-          'class': className,
-          'cx': elCenterX + (point.x - centerX) * 2 ** zoomLevel + 'px',
-          'cy': elCenterY + (point.y - centerY) * 2 ** zoomLevel + 'px',
-          'r': '4px',
-          'stroke': 'black',
-          'stroke-width': '0.5px',
-          'fill': 'lightgray',
-        }, 20003));
-        if (object.closed) SVGPoints += `${elCenterX + (point.x - centerX) * 2 ** zoomLevel},${elCenterY + (point.y - centerY) * 2 ** zoomLevel} `;
-        else {
-          if (previousPoint != null) {
-            returnValue[0].push(addSVGElement('line', {
+
+      if (object.linkedPoints.length != 0) {
+        let SVGPoints = '';
+        let lastPoint = [];
+
+        [...object.linkedPoints, ...(object.isPreview ? [null] : [])].forEach((hash, i) => {
+          let point = points[hash];
+
+          if (!(object.isPreview && i == object.linkedPoints.length)) returnValue[1].push(addSVGElement('circle', {
+            'class': className,
+            'cx': elCenterX + (point.x - centerX) * 2 ** zoomLevel + 'px',
+            'cy': elCenterY + (point.y - centerY) * 2 ** zoomLevel + 'px',
+            'r': '4px',
+            'stroke': 'black',
+            'stroke-width': '0.5px',
+            'fill': 'lightgray',
+          }, 20003));
+          
+          //プレビューしている線の場合、色を薄くする
+          if (object.isPreview && i == object.linkedPoints.length) {
+            //最後の線のみ別で描画
+            returnValue[0][1] = addSVGElement('line', {
               'class': className,
-              'x1': elCenterX + (previousPoint.x - centerX) * 2 ** zoomLevel + 'px',
-              'y1': elCenterY + (previousPoint.y - centerY) * 2 ** zoomLevel + 'px',
-              'x2': elCenterX + ((object.isPreview && i == object.linkedPoints.length ? object.previewX : point.x) - centerX) * 2 ** zoomLevel + 'px',
-              'y2': elCenterY + ((object.isPreview && i == object.linkedPoints.length ? object.previewY : point.y) - centerY) * 2 ** zoomLevel + 'px',
+              'x1': elCenterX + (lastPoint.x - centerX) * 2 ** zoomLevel + 'px',
+              'y1': elCenterY + (lastPoint.y - centerY) * 2 ** zoomLevel + 'px',
+              'x2': elCenterX + (object.previewX - centerX) * 2 ** zoomLevel + 'px',
+              'y2': elCenterY + (object.previewY - centerY) * 2 ** zoomLevel + 'px',
               'stroke': 'limegreen',
               'stroke-width': '4px',
-              'stroke-opacity': object.isPreview && i == object.linkedPoints.length ? '0.5' : '1',
-              'style': object.isPreview && i == object.linkedPoints.length ? 'pointer-events: none;' : '',
-            }, 20000));
+              'stroke-opacity': '0.5',
+              'style': 'pointer-events: none;',
+            }, 20000);
+          } else {
+            SVGPoints += `${elCenterX + (point.x - centerX) * 2 ** zoomLevel},${elCenterY + (point.y - centerY) * 2 ** zoomLevel} `;
+            lastPoint = { x: point.x, y: point.y };
           }
-          previousPoint = point;
+        });
+  
+        if (object.closed) {
+          returnValue[0][0] = addSVGElement('polygon', {
+            'class': className,
+            'points': SVGPoints,
+            'stroke': 'limegreen',
+            'stroke-width': '4px',
+            'fill': 'limegreen',
+            'fill-opacity': '0.3',
+          }, 20000);
+        } else {
+          returnValue[0][0] = addSVGElement('polyline', {
+            'class': className,
+            'points': SVGPoints,
+            'stroke': 'limegreen',
+            'stroke-width': '4px',
+            'fill': 'none',
+          }, 20000);
         }
-      });
-      if (object.closed) {
-        returnValue[0] = addSVGElement('polygon', {
-          'class': className,
-          'points': SVGPoints,
-          'stroke': 'limegreen',
-          'stroke-width': '4px',
-          'fill': 'limegreen',
-          'fill-opacity': '0.3',
-        }, 20000)
       }
+
       return returnValue;
     }
   }
