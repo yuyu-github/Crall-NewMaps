d414584c4aaa1890e7d17083b43a970849d137ba
yuyu <hcsd7b34@gmail.com>
2022-05-12 21:05:04 +0900

    Change: AddEventで要素を指定できるよう変更

--- renderer/js/right_click_menu/add_event.js
+++ renderer/js/right_click_menu/add_event.js
@@ -1,8 +1,11 @@
 import data from './data.js';
 import { display } from './right_click_menu.js';
 
-export function addEvent() {
-  for (let key in data) {
+export function addEvent(ids = []) {
+  if (ids.length == 0) ids = data;
+  for (let key in ids) {
+    if (!key in data) continue;
+
     document.querySelectorAll(key).forEach(el => {
       el.removeEventListener('contextmenu', display.bind(null, data[key]));
       el.addEventListener('contextmenu', display.bind(null, data[key]));
