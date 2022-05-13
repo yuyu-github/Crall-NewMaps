d3a4b6ceaf051fd6631eba896318ea5b7e23f7ee
yuyu <hcsd7b34@gmail.com>
2022-05-13 18:10:20 +0900

    Add: データ軽量化処理を追加

---
+++ renderer/js/project_file/elements_to_erase.js
@@ -0,0 +1,13 @@
+export const elementsToErase = {
+  'object': {
+    'isPreview': false,
+    'linkedTiles': [],
+    'previewX': 0,
+    'previewY': 0,
+    'borders': [],
+    'closed': true,
+  },
+  'point': {
+    'linkedObjects': [],
+  },
+}
---
+++ renderer/js/project_file/save.js
@@ -0,0 +1,49 @@
+import { background } from "../map/background/background.js";
+import { objects } from "../map/object/object.js";
+import { points } from "../map/point/point.js";
+import { elementsToErase } from "./elements_to_erase.js";
+
+const format = 1;
+
+export function save() {
+  //軽量化
+  let lightweightObjects = JSON.parse(JSON.stringify(objects)); //ディープコピー
+  let lightweightPoints = JSON.parse(JSON.stringify(points));
+  //objectsの軽量化
+  for (let item in lightweightObjects) {
+    //オブジェクト以外だった場合削除
+    if (toString.call(lightweightObjects[item]) != '[object Object]') {
+      delete lightweightObjects[item];
+      continue;
+    }
+
+    //不要な要素を削除
+    for (let el in lightweightObjects[item]) {
+      if (el in elementsToErase.object) {
+        delete lightweightObjects[item][el];
+        continue;
+      }
+    }
+  }
+  //pointsの軽量化
+  for (let item in lightweightPoints) {
+    //オブジェクト以外だった場合削除
+    if (toString.call(lightweightPoints[item]) != '[object Object]') {
+      delete lightweightPoints[item];
+      continue;
+    }
+
+    for (let el in lightweightPoints[item]) {
+      if (el in elementsToErase.point) {
+        delete lightweightPoints[item][el];
+        continue;
+      }
+    }
+  }
+
+  let data = {
+    'background': background,
+    'objects': lightweightObjects,
+    'points': lightweightPoints,
+  }
+}
