9f77bf7c28f68473e194e0abb54e0deb798565dc
yuyu <hcsd7b34@gmail.com>
2022-05-15 13:10:00 +0900

    Add: 開くと名前を付けて保存にショートカットキーを追加

--- menu.js
+++ menu.js
@@ -4,8 +4,8 @@ module.exports = mainWindow => [
   {
     label: 'ファイル',
     submenu: [
-      { label: '開く', click: () => mainWindow.webContents.send('load') },
-      { label: '名前を付けて保存', click: () => mainWindow.webContents.send('save') },
+      { label: '開く', click: () => mainWindow.webContents.send('load'), accelerator: 'CommandOrControl+O' },
+      { label: '名前を付けて保存', click: () => mainWindow.webContents.send('save'), accelerator: 'CommandOrControl+Shift+S' },
     ]
   }
 ]
