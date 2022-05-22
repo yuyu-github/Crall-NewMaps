702b8a33e62eb467e82fe6a468a748b63b7bd90d
yuyu <hcsd7b34@gmail.com>
2022-05-22 23:52:43 +0900

    Add: 拡張機能の管理ウインドウを追加

--- menu.js
+++ menu.js
@@ -7,5 +7,11 @@ module.exports = mainWindow => [
       { label: '保存', click: () => mainWindow.webContents.send('save', true), accelerator: 'CommandOrControl+S' },
       { label: '名前を付けて保存', click: () => mainWindow.webContents.send('save'), accelerator: 'CommandOrControl+Shift+S' },
     ]
+  },
+  {
+    label: '拡張機能',
+    submenu: [
+      { label: '拡張機能の管理', click: () => require('./create_subwindow.js')(mainWindow, 'extpack_mgmt') }
+    ]
   }
 ]
